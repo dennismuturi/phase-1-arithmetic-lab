9d169a460c0f5f272261e5dcd12d4b7772b0cfc0
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-16 12:31:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1=31;
+let num2=2;
+let num3=8;
+let num4=2;
+let num5=20;
+let num6=10;
 
-// Write your code here
+let multiply = parseInt(num1,10) * parseInt(num2,10);
+console.log(multiply);
+
+
+let random=Math.floor(Math.random() * 10);
+console.log(random);
+
+let mod = num3 / num4;
+console.log(mod);
+
+let max = (num5 > num6) ? num5 : num6;
+
